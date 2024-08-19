449773fa73d053e215643f84b7c562a30ab99ee5
Thikpad t480 <idwinursid@gmail.com>
2024-08-19 15:11:31 +0700

    twitter-fake

--- pages/api/posts/[postId].ts
+++ pages/api/posts/[postId].ts
@@ -11,7 +11,6 @@ export default async function handler(
 
   try {
     const { postId } = req.query;
-    console.log(postId);
     if (!postId || typeof postId !== "string") {
       throw new Error("Invalid ID");
     }
